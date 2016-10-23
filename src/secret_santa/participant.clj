a36a6538e60bc55cf979d24b89a8a5e28454da2f
Jacob Grismo <jacob@small-ponds.com>
2016-10-23 11:13:31 -0400

    fixes participant get by id

--- src/secret_santa/participant.clj
+++ src/secret_santa/participant.clj
@@ -4,11 +4,17 @@
             [secret_santa.db :refer [db]]
             [secret_santa.sql :refer [participants-all participant-by-id]]))
 
+(defn int-id [s] (Integer. s))
+
 (defroutes participant-routes
   ; list
   (GET "/api/participants" []
-    (response (participants-all db))))
+    (response (participants-all db)))
   ; get by id
   (GET "/api/participants/:user_id" [user_id]
-    (response (participant-by-id db {:id user_id})))
+    (response
+      (first
+        (participant-by-id db
+          {:id (int-id user_id)})))))
+
 
