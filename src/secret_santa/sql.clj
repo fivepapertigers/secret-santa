43a851cb58854aa6fb553d455f8f9ce67cfa4207
Jacob Grismo <jacob@small-ponds.com>
2016-10-23 14:57:10 -0400

    join excludes on  participants

--- src/secret_santa/sql.clj
+++ src/secret_santa/sql.clj
@@ -2,3 +2,20 @@
   (:require [hugsql.core :refer :all]))
 
 (def-db-fns "secret_santa/sql/participants.sql")
+
+(defn collect-relations [from-key to-key coll & {:keys [res] :or {res {}}}]
+  (if (empty? coll)
+    (vals res)
+    (let [coll (vec coll)
+          row (last coll)
+          id (row :id)
+          new_coll (pop coll)
+          rel_val (row from-key)
+          new_row
+            (if-let [curr (res id)]
+              (merge curr {to-key (conj (curr to-key) (row from-key))})
+              (merge (dissoc row from-key) {to-key (if rel_val [rel_val] [])}))]
+      (collect-relations from-key to-key new_coll :res (merge res {id new_row})))))
+
+(defn participants-all [db]
+  (collect-relations :exclude_id :excludes (participants-with-excludes db)))
--- src/secret_santa/sql/participants.sql
+++ src/secret_santa/sql/participants.sql
@@ -1,13 +1,14 @@
 -- src/secret_santa/sql/participants.sql
 -- Participants
 
--- :name participants-all :? :*
+-- :name participants-with-excludes :? :*
 -- :doc select all participants with all attributes
-SELECT *
+SELECT id, name, email, exclude_id, has
   FROM participants
+  LEFT JOIN excludes ON participants.id=excludes.participant_id
 
 -- :name participant-by-id :? :*
 -- :doc get artist info by music_brainz id
 SELECT *
   FROM participants
- WHERE id = :id
+  WHERE id = :id
