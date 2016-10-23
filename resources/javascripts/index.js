47b0540ea07c7fc8cf31c818ada5ce01ef2d7345
Jacob Grismo <jacob@small-ponds.com>
2016-10-23 11:12:47 -0400

    refactored participant as a store

--- resources/javascripts/index.js
+++ resources/javascripts/index.js
@@ -1,13 +1,51 @@
 (function(){
   var app = angular.module('SecretSanta', ['ngResource'])
 
-  app.service('ParticipantFactory', ['$resource', function($resource){
-    return $resource('/api/participants/:id', {id: '@id'})
-  }])
 
-  app.controller('HomeController', ['ParticipantFactory', function(ParticipantFactory){
-      var vm = this;
-      vm.participants = ParticipantFactory.query()
-    }])
+  // Participant Local Store
+  app.service('ParticipantStore', ['$resource', '$q', ParticipantStore]);
+  function ParticipantStore ($resource, $q){
+    var Participant = $resource('/api/participants/:id', {id: '@id'});
+
+    var participants = {};
+
+    return {
+      get: function (id) {
+        return $q(function(resolve, reject){
+          Participant.get({id: id}, function(participant){
+            resolve(updateParticipant(participant))
+          }, reject)
+        })
+      },
+      query: function(query_params) {
+        return $q(function(resolve, reject){
+          Participant.query(query_params, function(participants){
+            resolve(participants.map(updateParticipant));
+          }, reject)
+        })
+      }
+    }
+
+    function updateParticipant(participant) {
+      var id = participant.id;
+      participants[id] = participants[id] || {};
+      angular.copy(participant, participants[id])
+      return participants[id];
+    }
+
+  }
+
+  app.controller('ListParticipantsController', ['ParticipantStore',
+                                                ListParticipantsController])
+
+  function ListParticipantsController (ParticipantStore) {
+
+    var vm = this;
+
+    ParticipantStore.query().then(function(participants){
+      vm.participants = participants;
+    })
+
+  }
 
 })()
